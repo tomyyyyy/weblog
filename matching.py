139e962ed3711646f3531738653147b7b83784f9
tomyyyyy <1784489906@qq.com>
2021-06-06 20:31:56 +0800

    v1

--- matching.py
+++ matching.py
@@ -1,10 +1,16 @@
 import pymysql
+import configparser
 import re
 
 conn=pymysql.connect('localhost','root','password')
 conn.select_db('logger')
 cur=conn.cursor()
 table_name = 'logger_watcher_2021_05_03_09'
+cf = configparser.ConfigParser()
+cf.read(r"config.ini")
+ua_rule = cf.get("rule","ua_rule")
+method_rule = cf.get("rule","method_rule")
+sql_rule = cf.get("rule","sql_rule")
 
 with open("urlinfo.txt","w") as f:
     cur.execute("select ip, request_url,status_code,ua from " + table_name + ";")
@@ -13,7 +19,7 @@ with open("urlinfo.txt","w") as f:
         res=cur.fetchone()
         if res is None:
             break
-        if re.search("baidu|sougou|google|360",res[3], re.IGNORECASE):
+        if re.search(ua_rule,res[3], re.IGNORECASE):
             info = [res[2],res[0],res[1]]
             f.write(str(info)+"\n")
 
@@ -24,7 +30,7 @@ with open("urlinfo.txt","w") as f:
         res=cur.fetchone()
         if res is None:
             break
-        if re.search("GET|POST",res[3], re.IGNORECASE) == None:
+        if re.search(method_rule,res[3], re.IGNORECASE) == None:
             print (res[2],res[0],res[1],res[3])
             info = [res[2],res[0],res[1],res[3]]
             f.write(str(info)+"\n")
@@ -35,7 +41,7 @@ with open("urlinfo.txt","w") as f:
         res=cur.fetchone()
         if res is None:
             break
-        if re.search("\{*.*?\}|>|<|\||information_schema|select",res[1], re.IGNORECASE):
+        if re.search(sql_rule,res[1], re.IGNORECASE):
             print (res[2],res[0],res[1])
             info = [res[2],res[0],res[1]]
             f.write(str(info)+"\n")
